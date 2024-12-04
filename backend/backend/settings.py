c1e1154b66fa7afa5f108f38a9f5c60b039c3bbd
Ewa Soltysik <63466233+ewasol@users.noreply.github.com>
2024-12-04 17:11:51 +0100

    add web app & db hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-u+t9vn016lc2ej0))+d6f0k&0=%84l_m$o4tb7zam*@z9$y1c!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ewasol.eu.pythonanywhere.com"]
 
 
 # Application definition
@@ -90,11 +90,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '3306',
     }
 }
+# if DEBUG:
+#     DATABASES['default']['HOST'] = '127.0.0.1'
 
 
 # Password validation
